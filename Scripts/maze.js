1b7fff98e11a71c61d16be79904ba2cbeaddf42e
Neon <darkneonium@gmail.com>
2021-12-22 19:30:51 +0000

    add neighbours to tiles

--- Scripts/maze.js
+++ Scripts/maze.js
@@ -1,9 +1,12 @@
+const directions = ["UP", "RIGHT", "DOWN", "LEFT"]
+
 class Tile {
   constructor(x, y) {
     this.x = x
     this.y = y
 
     this.paths = new Set();
+    this.neighbours = new Set();
   }
 }
 
@@ -16,13 +19,31 @@ class MazeGenerator {
 
     for (let x in range(1, width)) {
       for (let y in range(1, height)) {
-        this.tiles.set([x, y], new Tile(x, y));
+        let pos = [x, y].map(Number)
+        console.log(pos)
+        this.tiles.set(pos, new Tile(x, y))
+
+        let tile = this.tiles.get(pos)
+        console.log(this.tiles)
+        console.log(tile)
+
+        if (x != 0) tile.neighbours.add([pos[0] - 1, pos[1]]);
+        if (x != width - 1) tile.neighbours.add([pos[0] + 1, pos[1]]);
+        if (y != 0) tile.neighbours.add([pos[0], pos[1] - 1]);
+        if (y != height - 1) tile.neighbours.add([pos[0], pos[1] + 1]);
       }
     }
+    
+    let pivot = [
+      Math.floor(width / 2),
+      Math.floor(height / 2)
+    ];
+
+
   }
 }
 
-maze = new MazeGenerator(3, 3)
+let maze = new MazeGenerator(3, 3)
 console.log(maze.tiles)
 
 function range(start, end) {
