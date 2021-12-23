69c6345e512b95015054ab68d8db8c460b95d7bf
Neon <darkneonium@gmail.com>
2021-12-23 21:16:04 +0000

    maze rendering - don't mind all of the messy code, i'll clean it up later

--- Scripts/maze.js
+++ Scripts/maze.js
@@ -58,21 +58,21 @@ class MazeGenerator {
     while (connectQueue.length > 0) {
 
       let currentTile = this.getTile(connectQueue[0]);
-      currentTile.isActive = true
+      currentTile.isActive = true;
       connectQueue.shift();
 
       let connectedTiles = [];
       currentTile.neighbours.forEach(t => {
-        let u = this.getTile(t)
-        u.isActive ? connectedTiles.push(u) : connectQueue.push(t)
+        let u = this.getTile(t);
+        u.isActive ? connectedTiles.push(u) : connectQueue.push(t);
       });
-      let randomTileNumber = Math.floor(Math.random(0, connectedTiles.length - 1))
-      this.connectTiles(currentTile, connectedTiles[randomTileNumber])
+      let randomTileNumber = Math.floor(Math.random(0, connectedTiles.length - 1));
+      this.connectTiles(currentTile, connectedTiles[randomTileNumber]);
     };
   };
 
   connectTiles(tile, target) {
-    if (tile.connections.indexOf(target) !== -1) return
+    if (tile.connections.indexOf(target) !== -1) return;
     // Add connections
     tile.connections.push(target);
     target.connections.push(tile);
@@ -87,7 +87,7 @@ class MazeGenerator {
 };
 
 let maze = new MazeGenerator(6, 6);
-console.log(maze.tiles);
+renderMaze(maze);
 
 function range(start, end) {
   return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
@@ -101,6 +101,42 @@ function sumArrays(arr1, arr2) {
 
 function subtractArrays(arr1, arr2) {
   return arr1.map((num, idx) => {
-    return num - arr2[idx]
+    return num - arr2[idx];
   });
 };
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function draw(file, x, y) {
+  const ctx = document.getElementById('gameCanvas').getContext('2d');
+  const img = new Image();
+  img.onload = () => {
+    ctx.drawImage(img, x, y);
+  };
+  img.src = file;
+}
+
+function renderMaze(maze) {
+  let remainingTiles = maze.tiles;
+  for (y in remainingTiles) {
+    for (x in remainingTiles[y]) {
+      let n = 0;
+      let img = "./Sprites/Tiles/";
+      if (remainingTiles[y][x].directions.findIndex(a => arraysEqual(a, UP)) !== -1) n += 1;
+      if (remainingTiles[y][x].directions.findIndex(a => arraysEqual(a, RIGHT)) !== -1) n += 2;
+      if (remainingTiles[y][x].directions.findIndex(a => arraysEqual(a, DOWN)) !== -1) n += 4;
+      if (remainingTiles[y][x].directions.findIndex(a => arraysEqual(a, LEFT)) !== -1) n += 8;
+      img += n.toString() + ".png";
+      draw(img, x * 30, y * 30);
+    }
+  }
+}
