44cd2706a7a55201cc6edbb578b5ff37b51788bf
Neon <57234886+EnbyDaze@users.noreply.github.com>
2022-08-23 18:31:59 +0000

    not so super

---
+++ Scripts/classes-new.js
@@ -0,0 +1,43 @@
+class Sprite {
+  contructor(img) {
+    this.img = img
+  }
+
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
+}
+
+class GameObject {
+  position = {
+    x: 0,
+    y: 0
+  }
+  constructor(x, y, sprite) {
+    this.position.x = x;
+    this.position.y = y;
+    this.sprite = sprite
+  }
+}
+
+class MovingObject extends GameObject {
+  velocity = {
+    x: 0,
+    y: 0
+  }
+
+  constructor(x, y, sprite) {
+    super(x, y, sprite)
+  }
+
+  modifyVelocity(v) {
+    this.velocity.x += v.x
+    this.velocity.y += v.y
+  }
+}
+
+class Ball extends MovingObject {
+  contructor(x, y, sprite) {
+    super(x, y, sprite)
+  }
+}
--- Scripts/game.js
+++ Scripts/game.js
@@ -1,2 +1,9 @@
-let maze = new Maze(15, 15);
-renderMaze(maze);
+//let maze = new Maze(15, 15);
+//renderMaze(maze);
+
+let ball = new Ball(0, 0, "img here")
+
+ball.modifyVelocity({x: 3, y:2})
+
+console.log(ball.velocity.x)
+console.log(ball.velocity.y)
