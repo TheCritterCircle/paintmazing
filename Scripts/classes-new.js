66d09fd406c99ee721648e6541ccf292f1997474
Neon <darkneonium@gmail.com>
2022-08-23 22:08:30 +0000

    more classes and stuff

--- Scripts/classes-new.js
+++ Scripts/classes-new.js
@@ -1,19 +1,29 @@
 class Sprite {
-  contructor(img) {
+  position = DEFAULT_POSITION
+
+  contructor(img, {pos: {x, y}, width, height}) {
     this.img = img
+
+    this.position.x = pos[x] ?? 0
+    this.position.y = pos[y] ?? 0
+
+    this.width = width ?? img.width
+    this.height = height ?? img.height
   }
 
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
   }
+
+  scale({width, height}) {
+    this.width = width ?? this.width
+    this.height = height ?? this.height
+  }
 }
 
 class GameObject {
-  position = {
-    x: 0,
-    y: 0
-  }
-  constructor(x, y, sprite) {
+  position = DEFAULT_POSITION
+  constructor({x, y}, sprite) {
     this.position.x = x;
     this.position.y = y;
     this.sprite = sprite
@@ -26,11 +36,29 @@ class MovingObject extends GameObject {
     y: 0
   }
 
-  modifyVelocity(v) {
-    this.velocity.x += v.x
-    this.velocity.y += v.y
+  modifyVelocity(vel) {
+    this.velocity.x += vel.x
+    this.velocity.y += vel.y
   }
 }
 
 class Ball extends MovingObject {
+  // ball stuff here idk
+}
+
+class Tile {
+  position = DEFAULT_POSITION
+
+  sprite = null
+
+  constructor({x, y}) {
+    this.position.x = x
+    this.position.y = y
+  }
+
+  applySprite(sprite) {
+    sprite.scale(TILE_DIMENSIONS)
+
+    this.sprite = sprite
+  }
 }
